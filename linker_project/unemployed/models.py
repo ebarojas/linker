f2d57028efb93a81c16400728bc822c5caa45b2c
safv12 <ing.fran12@gmail.com>
2015-11-25 19:26:02 -0600

    adds linkedin fields

--- linker_project/unemployed/models.py
+++ linker_project/unemployed/models.py
@@ -1,6 +1,12 @@
 from users.models import User
+from django.db import models
 
 # Create your models here.
 class Unemployed(User):
+    linkedin_id = models.CharField(max_length=255)
+    headline = models.CharField(max_length=255)
+    num_connections = models.IntegerField()
+    picture_url = models.CharField(max_length=320)
+
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
--- linker_project/users/models.py
+++ linker_project/users/models.py
@@ -6,7 +6,7 @@ class User(AbstractBaseUser):
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
     email = models.EmailField(max_length=320)
-    profile_picture = models.ImageField(max_length=320)
+    # profile_picture = models.ImageField()
     phone = models.CharField(max_length=320)
     location = models.CharField(max_length=320)
 
