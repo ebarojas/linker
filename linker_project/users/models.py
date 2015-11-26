c3383ebfa6c55c721d707783b8ec6cef3edd1c4d
safv12 <ing.fran12@gmail.com>
2015-11-25 20:24:08 -0600

    updates location to lat and lon

--- linker_project/users/models.py
+++ linker_project/users/models.py
@@ -7,7 +7,8 @@ class User(AbstractBaseUser):
     last_name = models.CharField(max_length=80)
     email = models.EmailField(max_length=320)
     phone = models.CharField(max_length=320)
-    location = models.CharField(max_length=320)
+    location_lat = models.FloatField()
+    location_lon = models.FloatField()
     role = models.IntegerField(choices=role_choices, default=1)
 
     USERNAME_FIELD = 'email'
