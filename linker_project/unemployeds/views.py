2656752554d8a8bca64754664d5a6aa10f8548c2
Everardo Barojas <everardo@volabit.com>
2015-12-02 17:21:57 -0600

    Add working vacant views

--- linker_project/unemployeds/views.py
+++ linker_project/unemployeds/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.views.generic import View
 
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
+# from headhunters.models import Headhunter # Maybe remove this
+from headhunters.models import Vacant
 
 class UnemployedHome(View):
     def get(self,request):
-        return render(request, 'unemployed/vacants_slide.html')
+        vacants = listing(request)
+        return render_to_response('unemployed/vacants_slide.html', {"vacants": vacants})
+
+def listing(request):
+    vacant_list = Vacant.objects.all()
+    paginator = Paginator(vacant_list, 1) # Show 1 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        vacants = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        vacants = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        vacants = paginator.page(paginator.num_pages)
+
+    return vacants
