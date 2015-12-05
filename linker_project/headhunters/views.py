0dcabfaf405ecd42959c220bdd6924e1ae5e3bbb
safv12 <ing.fran12@gmail.com>
2015-12-04 20:00:36 -0600

    Add save like functions

--- linker_project/headhunters/views.py
+++ linker_project/headhunters/views.py
@@ -6,23 +6,35 @@ from django.core.paginator import Paginator
 from django.core.paginator import EmptyPage
 from django.core.paginator import PageNotAnInteger
 from unemployeds.models import Unemployed
+from headhunters.models import Vacant
+from matches.models import VacantLike
 
 class HeadhunterHome(View):
     def get(self,request):
         users = listing(request)
-        return render_to_response('headhunter/users_slides.html', {"users": users})
+        return render(request, 'headhunter/users_slides.html', {"users": users})
+
+    def post(self, request):
+        vacant = Vacant.objects.get(id=2) # Cambiar por sesion de usuario
+        user = Unemployed.objects.get(id=request.POST.get('userId'))
+        like = VacantLike(vacant=vacant, unemployed=user)
+        like.save()
+
+        users = listing(request)
+        return render(request, 'headhunter/users_slides.html', {"users": users})
+
 
 
 def listing(request):
     user_list = Unemployed.objects.all()
     paginator = Paginator(user_list, 1)
-    page = request.GET.get('page')
+    page = request.GET.get('page') if request.GET.get('page') else request.POST.get('page')
 
     try:
-        vacants = paginator.page(page)
+        users = paginator.page(page)
     except PageNotAnInteger:
-        vacants = paginator.page(1)
+        users = paginator.page(1)
     except EmptyPage:
-        vacants = paginator.page(paginator.num_pages)
+        users = paginator.page(paginator.num_pages)
 
-    return vacants
+    return users
