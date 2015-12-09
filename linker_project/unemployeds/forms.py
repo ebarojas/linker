8498845d4542d6eeeff60d0d47113a1a008f003a
safv12 <ing.fran12@gmail.com>
2015-12-08 18:12:49 -0600

    Add form file

---
+++ linker_project/unemployeds/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from unemployeds.models import Unemployed
+
+
+class Signup(forms.ModelForm):
+    class Meta:
+        model = Unemployed
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'phone',
+            'location',
+            'headline',
+            'picture_url'
+        ]
+        
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Write your first name'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Last name'
+            }),
+            'email': forms.EmailInput(attrs={
+                'placeholder': 'email@exaple.com'
+            }),
+            'phone': forms.TextInput(attrs={
+                'placeholder': '10 digits please'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': '****************'
+            }),
+        }
